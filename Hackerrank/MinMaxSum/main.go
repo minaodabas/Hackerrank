b12beebc14fb6571990873d529388548c9b4f6ee
mina.odabas <mina.odabas@hepsiburada.com>
2021-02-14 01:07:07 +0300

    Initial Commit

---
+++ Hackerrank/MinMaxSum/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+
+)
+func main() {
+	a := make([]int, 5)
+
+	fmt.Scan(&a[0], &a[1], &a[2], &a[3], &a[4])
+	sort.Ints(a)
+	min := a[0] + a[1] + a[2] + a[3]
+	max := a[1] + a[2] + a[3] + a[4]
+	fmt.Println(min, max)
+}
